c2f33da590150bb4c9025884b404445366a71e19
dotNoober <dotnoober@gmail.com>
2024-01-08 21:53:38 -0500

    removed most jQuerry

--- ipad.js
+++ ipad.js
@@ -1,35 +1,75 @@
-var ipad = navigator.userAgent.match(/iPad/i) != null;
+var ipad = navigator.userAgent.match(/iPad/i) !== null;
 
 if (ipad) {
-  $(function () {
-    $('#left-controls, #right-controls').show();
-    $('body > *').hide();
-    $('body').css('margin', '0px').css('background', 'black').prepend($('#game-container').remove());
-    $('#game-container').width(1024).css('margin-top', 26).show();
-    $('#canvas').attr('width', 1020).attr('height', 660).css('background', 'white').css('margin', '0 1');
+  document.addEventListener('DOMContentLoaded', function () {
+    var leftControls = document.getElementById('left-controls');
+    var rightControls = document.getElementById('right-controls');
+    var bodyChildren = document.body.children;
+    var gameContainer = document.getElementById('game-container');
+    var canvas = document.getElementById('canvas');
 
-    $('head').prepend($('<meta/>').attr('name', 'viewport').attr('content', 'width=device-width; height=device-height; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;'));
+    leftControls.style.display = 'block';
+    rightControls.style.display = 'block';
 
-    $('#left-controls, #right-controls').bind('touchstart touchmove touchend', function (e) {
-      if (e.type != 'touchend') {
-        for (k in KEY_STATUS) {
-          KEY_STATUS[k] = false;
-        }
-      }
-      var touches = e.type == 'touchend' ? e.originalEvent.changedTouches : e.originalEvent.touches
-      for (var i = 0; i < touches.length; i++) {
-        var ele = document.elementFromPoint(touches[i].pageX, touches[i].pageY);
-        KEY_STATUS[ele.id] = (e.type != 'touchend');
-      }
+    // Hide all body children
+    for (var i = 0; i < bodyChildren.length; i++) {
+      bodyChildren[i].style.display = 'none';
+    }
+
+    document.body.style.margin = '0px';
+    document.body.style.background = 'black';
+    document.body.insertBefore(gameContainer, document.body.firstChild);
+    gameContainer.style.width = '1024px';
+    gameContainer.style.marginTop = '26px';
+    gameContainer.style.display = 'block';
+
+    canvas.width = 1020;
+    canvas.height = 660;
+    canvas.style.background = 'white';
+    canvas.style.margin = '0 1';
+
+    var metaViewport = document.createElement('meta');
+    metaViewport.name = 'viewport';
+    metaViewport.content = 'width=device-width; height=device-height; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;';
+    document.head.insertBefore(metaViewport, document.head.firstChild);
+
+    var controls = [leftControls, rightControls];
+
+    controls.forEach(function (control) {
+      control.addEventListener('touchstart', handleTouch);
+      control.addEventListener('touchmove', handleTouch);
+      control.addEventListener('touchend', handleTouch);
     });
 
-    $(document).bind('touchstart', function (e) {
+    document.addEventListener('touchstart', function () {
       window.gameStart = true;
     });
 
-    $(document).bind('gesturestart gesturechange gestureend touchstart touchmove touchend', function (e) {
+    document.addEventListener('gesturestart', preventDefault);
+    document.addEventListener('gesturechange', preventDefault);
+    document.addEventListener('gestureend', preventDefault);
+    document.addEventListener('touchstart', preventDefault);
+    document.addEventListener('touchmove', preventDefault);
+    document.addEventListener('touchend', preventDefault);
+
+    function handleTouch(e) {
+      if (e.type !== 'touchend') {
+        for (var k in KEY_STATUS) {
+          KEY_STATUS[k] = false;
+        }
+      }
+
+      var touches = e.type === 'touchend' ? e.changedTouches : e.touches;
+
+      for (var i = 0; i < touches.length; i++) {
+        var ele = document.elementFromPoint(touches[i].pageX, touches[i].pageY);
+        KEY_STATUS[ele.id] = (e.type !== 'touchend');
+      }
+    }
+
+    function preventDefault(e) {
       e.preventDefault();
-    });
+    }
   });
 }
 
